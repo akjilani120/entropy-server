d3ef42d73f5b5dc13c71dbb69241eb667d4cc856
unknown <abdulkaderjilani120@gmail.com>
2022-09-05 01:34:03 +0600

    add blog data

--- index.js
+++ index.js
@@ -2,20 +2,41 @@ const express = require('express')
 const app = express()
 const cors = require ("cors")
 const port = process.env.PORT || 5000
-require('dotenv').config()
-app.use(cors())
-app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.uddhpex.mongodb.net/?retryWrites=true&w=majority`;
+require('dotenv').config()
+const uri = `mongodb+srv://akjilani:Rp7pHWKAN3kd9ibC@cluster0.uddhpex.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run() {
-  try{
-    await client.connect();
-    const techCollection = client.db("blog").collection("tech");
+app.use(cors())
+app.use(express.json())
 
+async function run() {
+  
+  try{   
+    await client.connect();
+
+    const blogCollection = client.db("blog").collection("tech");
+    
+    app.get("/blog/all",   async (req , res)=>{     
+     const result = await blogCollection.find().toArray()  
+     res.send(result)
+    })
+
+    app.get("/blog",   async (req , res)=>{
+      const blogType = req.query.blogType         
+      const query ={ blogType : blogType}       
+     const result = await blogCollection.find(query).toArray()    
+     res.send(result)
+    })
+
+    app.post('/blog', async(req, res) =>{
+      const blog = req.body
+      console.log(blog)
+      const result = await blogCollection.insertOne(blog)
+      res.send(result)
+    })
   }finally{
-    await client.close();
+    
   }
 }
 run().catch(console.dir);
